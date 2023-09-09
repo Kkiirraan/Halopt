e8c953a7be81a7fa232aef4e468ea1d3b08f4cf6
Abhiraj B <abhi68130@gmail.com>
2023-09-09 17:41:05 +0530

    abhiraj

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def login_user(request):
+    return render(request,'main\login.html')
+
+#User register view
+def register_user(request):
+    return render(request,'register.html')
