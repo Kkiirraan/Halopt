c6b656aa8e0cb17f006aec18355b1fca1878641d
Abhiraj B <abhi68130@gmail.com>
2023-09-10 20:47:57 +0530

    register page addedd

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 from django.urls import path
 from . import views
@@ -6,14 +5,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_user,name='login'),
-    path('register/', views.register_user, name='register'),
+    path('register', views.register_user, name='register'),
 ]
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("login_user",views.login_user,name="login_user"),
-    
-]
->>>>>>> d7dc0b3ba0b5a4946a8774b0d4afb0d8a63ec7a1
--- main/views.py
+++ main/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
-<<<<<<< HEAD
 def login_user(request):
-    return render(request,'main\login.html')
+    return render(request,'main\\login.html')
 
 #User register view
 def register_user(request):
-    return render(request,'register.html')
-=======
-def login_user():
-    pass
->>>>>>> d7dc0b3ba0b5a4946a8774b0d4afb0d8a63ec7a1
+    return render(request,'main\\register1.html')
